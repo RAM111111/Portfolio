e7843e0053e2775d520cc6228534a5d8fce52eb6
RAM111111 <r.r.99@windowslive.com>
2021-01-14 15:02:59 +0300

    footer & media

---
+++ app.js
@@ -0,0 +1,8 @@
+const hum_icon = document.querySelector('#nav-bar .nav-bar .nav-list .humburger');
+const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
+
+
+
+hum_icon.addEventListener('click' , ()=>{
+nav_list.classList.toggle('open');
+});
